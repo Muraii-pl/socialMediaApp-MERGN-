9cf04a0c4bd3446da8ed4c1ab3a39a7a5231b31e
Muraii-pl <mateusz.glowania@gmail.com>
2021-08-05 21:54:48 +0200

    add others file

---
+++ index.js
@@ -0,0 +1,24 @@
+const {ApolloServer} = require('apollo-server')
+const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const resolvers = require('./graphql/resolvers')
+const typeDefs = require('./graphql/typeDefs')
+
+
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context:({req}) => ({req})
+})
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true,useUnifiedTopology:true}).then(() => {
+    return server.listen({port: 5000})
+})
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    })
