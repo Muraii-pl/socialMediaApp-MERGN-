5da93b9421bc388ede80874909d924b0b4e8b0c1
Muraii-pl <mateusz.glowania@gmail.com>
2021-08-16 07:41:32 +0200

    React - Create Post

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import App from './App'
-import { InMemoryCache, ApolloClient,createHttpLink,ApolloProvider }from '@apollo/client'
+import { InMemoryCache, ApolloClient,createHttpLink,ApolloProvider, }from '@apollo/client'
+import {setContext} from "@apollo/client/link/context";
 
 const httplink = createHttpLink({
     uri:'http://localhost:5000'
 })
 
+const authLink = setContext(() => {
+    const token = localStorage.getItem('jwtToken')
+    return{
+        headers:{
+            Authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
 const client = new ApolloClient({
-    link:httplink,
+    link:authLink.concat(httplink),
     cache: new InMemoryCache()
 })
+
 
 const ApolloProviders = () => {
   return(
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useQuery} from "@apollo/client";
-import gql from 'graphql-tag'
 import {Grid} from "semantic-ui-react";
 import PostCard from '../components/PostCard'
-
+import {AuthContext} from "../context/auth";
+import PostForm from '../components/PostForm'
+import {FETCH_POST_QUERY} from "../util/graphql";
 
 const Home = () => {
+    const {user} = useContext(AuthContext)
     const {loading, data} = useQuery(FETCH_POST_QUERY)
     return (
         <Grid columns={3}>
@@ -13,6 +15,11 @@ const Home = () => {
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
+                {user && (
+                    <Grid.Column>
+                        <PostForm></PostForm>
+                    </Grid.Column>
+                )}
                 {loading ? (<h1>Loading posts...</h1>) : (
                     data.getPosts && data.getPosts.map(post => (
                         <Grid.Column key={post.id} style={{marginBottom:20}}>
@@ -25,19 +32,6 @@ const Home = () => {
     );
 };
 
-const FETCH_POST_QUERY = gql`
-    {
-        getPosts {
-            id body createdAt username likeCount
-            likes{
-                username
-            }
-            commentCount
-            comments{
-                id username createdAt body
-            }
-        }
-    }
-`
+
 
 export default Home;
